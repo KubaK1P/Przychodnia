e0d479bc1209ef5e7a77d2e19befd8fb9246d8dd
KubaK1P <1p22kus@gmail.com>
2024-11-18 18:57:30 +0100

    fix: (will not be used)

--- przychodnia/src/app/components/loadingGIF/index.tsx
+++ przychodnia/src/app/components/loadingGIF/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export const LoadingGIF = ({ hidden }: { hidden?: boolean }) => (
   <Image
-    src="/public/loading.gif"
+    src="/loading.gif"
     hidden={hidden}
     width={100}
     height={100}
