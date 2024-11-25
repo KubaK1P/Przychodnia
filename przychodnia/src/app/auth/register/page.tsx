40942fd8c90e319be1287534d0e314eff7d408aa
KubaK1P <1p22kus@gmail.com>
2024-11-25 09:12:58 +0100

    fix: another quick frontend fix

--- przychodnia/src/app/auth/register/page.tsx
+++ przychodnia/src/app/auth/register/page.tsx
@@ -2,7 +2,7 @@ import { RegisterForm } from "@app/app/components/form";
 
 export default function Register() {
   return (
-    <main className="w-full flex flex-wrap h-[90vh] shadow-md">  
+    <main className="w-full flex flex-wrap min-h-[90vh] shadow-md">  
           <RegisterForm />
     </main>
   );
