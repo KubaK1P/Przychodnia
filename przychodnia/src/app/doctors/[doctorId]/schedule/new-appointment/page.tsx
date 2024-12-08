f946676e5eb44aa6106ee77038947c8d52070452
KubaK1P <1p22kus@gmail.com>
2024-12-08 16:45:16 +0100

    feat(schedule_appointment): FrOnTEnD ahh form

--- przychodnia/src/app/doctors/[doctorId]/schedule/new-appointment/page.tsx
+++ przychodnia/src/app/doctors/[doctorId]/schedule/new-appointment/page.tsx
@@ -27,7 +27,7 @@ export default async function Page() {
 
 
 
-  return <form action={async (d: FormData) => {
+  return <main className="min-h-[80vh] w-full flex flex-col justify-center items-center"><form className="basis-1/2 w-[60%] p-4 rounded-md shadow-md bg-white mb-4 flex flex-col justify-evenly gap-4" action={async (d: FormData) => {
     "use server"
     const db_settings: IDBSettings = GetDBSettings();
     const connection = await mysql.createConnection(db_settings);
@@ -36,15 +36,16 @@ export default async function Page() {
 
 
   }}>
-    <input type="date" name="data_wizyty" />
-    <input type="text" name="powod_wizyty" placeholder="powód wizyty?" />
-    <select name="status_wizyty">
+    <h1 className="text-2xl p-2">Umów wizytę</h1>
+    <input type="date" name="data_wizyty" className="p-2 border-2 rounded-md" />
+    <input type="text" name="powod_wizyty" placeholder="powód wizyty?" className="p-2 border-2 rounded-md" />
+    <select name="status_wizyty" className="p-2 border-2 rounded-md">
       <option value="zaplanowana">zaplanowana</option>
       <option value="odwolana">odwołana</option>
       <option value="zakonczona">zakończona</option>
     </select>
-    lekasz
-    <select name="id_lekarza">
+    Lekarz:
+    <select name="id_lekarza" className="p-2 border-2 rounded-md">
       {
         lekarze.map(lekarz =>
           <option key={lekarz.id_lekarza} value={lekarz.id_lekarza}>{lekarz.imie} {lekarz.nazwisko}</option>
@@ -53,6 +54,6 @@ export default async function Page() {
     </select>
 
 
-    <input type="submit" />
-  </form>
+    <input type="submit" className="p-2 border-2 rounded-md hover:bg-gray-200" />
+  </form></main>
 }
