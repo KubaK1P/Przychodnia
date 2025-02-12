8c36284ab67b618b3172891a26cee729d51ba3f4
KubaK1P <1p22kus@gmail.com>
2025-02-12 20:35:23 +0100

    fix(ads): frontend

--- przychodnia/src/app/auth/register/page.tsx
+++ przychodnia/src/app/auth/register/page.tsx
@@ -1,9 +1,18 @@
 import { RegisterForm } from "@app/app/components/form";
-
+import Image from "next/image";
 export default function Register() {
   return (
-    <main className="w-full flex flex-wrap min-h-[90vh] shadow-md">  
-          <RegisterForm />
+    <main className="w-full flex relative flex-wrap min-h-[100vh]  shadow-md">
+      <div className="absolute image-wrapper min-h-[100vh] z-[100]">
+        <Image
+          src="/przychodnia2.png"
+          alt="main background"
+          width="1920"
+          height="953"
+          className="h-auto"
+          style="aspect-ratio: initial" />
+      </div>
+      <RegisterForm />
     </main>
   );
 }
--- przychodnia/src/app/components/header/index.tsx
+++ przychodnia/src/app/components/header/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { getCookie } from "@app/app/lib/cookies";
 import { headers } from "next/headers";
 // import { useEffect } from "react";
-
+import Image from "next/image";
 // export interface registrationGETResponseInterface {
 //   userExists: string
 // }
@@ -27,7 +27,13 @@ export const Header = async () => {
 
 
   return (
-    <header className="basis-[100%] w-full fixed top-0 flex flex-row items-center p-6 gap-2 bg-[#ffffff55] shadow-md rounded-md z-1000" id="header">
+    <header className="basis-[100%] w-full fixed top-0 flex flex-row items-center p-6 gap-2 bg-[#ffffff55] shadow-md rounded-md z-[1000]" id="header">
+      <Image
+        src="/logo.png"
+        alt="logo"
+        width="60"
+        height="60">
+      </Image>
       <Link href="/" className="font-xl text-2xl font-semibold text-slate-950 hover:underline mr-auto tracking-wide p-3">
         Przychodnia
       </Link>
@@ -35,7 +41,7 @@ export const Header = async () => {
         <>
           <Link
             href="/profile"
-            className=" p-1 rounded-md"
+            className=" p-1 rounded-md hover:text-blue-300"
           >
             Twoja tablica
           </Link>
@@ -45,7 +51,7 @@ export const Header = async () => {
           </div>
           <Link
             href="/auth/logout"
-            className=" p-1 rounded-md"
+            className=" p-1 rounded-md hover:text-blue-300"
           >
             Wyloguj się
           </Link>
@@ -55,13 +61,13 @@ export const Header = async () => {
         <>
           <Link
             href="/auth/login"
-            className="rounded-md p-3"
+            className="rounded-md p-3 hover:text-blue-300"
           >
             Zaloguj się
           </Link>
           <Link
             href="/auth/register"
-            className="rounded-md p-3"
+            className="rounded-md p-3 hover:text-blue-300"
           >
             Zarejestruj się
           </Link>
